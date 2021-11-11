03d4361512461a2f541bebf31d9940c3a168fd41
Aleem Rehmtulla <aleem@rehmtulla.com>
2021-11-11 15:43:09 -0500

    Card MVP ✅

--- src/components/Players/Tag.js
+++ src/components/Players/Tag.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
+import styled from "styled-components";
 
+const Tag1 = styled.div`
+  background-color: black;
+  color: white;
+  font-size: 14px;
+  display: inline;
+  padding: 4px 5px;
+  border-radius: 5px;
+  margin: 10px 0px;
+`;
 
 export class Tag extends Component {
   render() {
     return (
-        <div class="rectangle">
-            Hi
-        </div>
+        <Tag1>PadawanDAO</Tag1>
     );
   }
 }
