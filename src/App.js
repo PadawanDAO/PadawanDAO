ede20a27b5935b77edbbddeb37bf5b3d9c674f74
Aleem Rehmtulla <aleem@rehmtulla.com>
2021-11-11 01:33:50 -0500

    Super basic nav

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Navigation, Home, Players, Proposals } from "./pages";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navigation />
+        <Switch>
+          <Route path="/" exact component={() => <Home />} />
+          <Route path="/proposals" exact component={() => <Proposals />} />
+          <Route path="/players" exact component={() => <Players />} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Players.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Players() {
+  return (
+    <div className="about">
+      <div class="container">
+        <div class="row align-items-center my-5">
+          <div class="col-lg-5">
+            <h1 class="font-weight-light">Players</h1>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Players;
